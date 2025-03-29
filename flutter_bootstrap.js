4e460274524e3eaa4c19a8f0492108e6e50b9262
赛博宠迹 <cyberpaws@saibochacStudio.home>
2025-03-29 23:30:31 +0100

    Fix 'From', 'To'

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1174963029"
+    serviceWorkerVersion: "1117174382"
   }
 });
